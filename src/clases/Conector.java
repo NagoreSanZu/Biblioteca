e7ce3edf481def15205b060d4c43ebed823dd00b
nagore <znagoresanchez@gmail.com>
2023-01-26 12:29:44 +0100

    conexion

--- src/clases/Conector.java
+++ src/clases/Conector.java
@@ -1,5 +1,22 @@
 package clases;
 
-public class Conector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Conector {
+	private static final String HOST = "localHost";
+	private static final String BBDD ="eh_garden";
+	private static final String USERNAME = "root";
+	private static final String PASSWROD = "";
+	
+	
+	public void Conexion() throws ClassNotFoundException, SQLException {
+		Connection conexion;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conexion= DriverManager.getConnection("jdbc:mysql://"+HOST+"/"+BBDD, USERNAME, PASSWROD);
+		Statement st = conexion.createStatement();
+	}
+	
 }
