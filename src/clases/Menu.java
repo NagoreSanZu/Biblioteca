17cb0d9c7c6588c2629cb2ed0a0c59c16111b2eb
nagore <znagoresanchez@gmail.com>
2023-01-26 12:19:36 +0100

    menu libros

--- src/clases/Menu.java
+++ src/clases/Menu.java
@@ -26,8 +26,10 @@ public class Menu {
 			case OPCION_GESTIONAR:
 				
 				break;
+				
 			case OPCION_SOCIOS:
-				System.out.println("segunda opcion seleccionada\n");
+				menuLibros(scan);
+				
 				break;
 			case OPCION_PRESTAMOS:
 				System.out.println("tercera opcion seleccionada\n");
@@ -41,6 +43,43 @@ public class Menu {
 
 		} while (opcion_menu != SALIR);
 		scan.close();
+	}
+
+	public void menuLibros(Scanner scan) {
+		final int OPCION_INSERTAR = 1;
+		final int OPCION_ELIMINAR = 2;
+		final int OPCION_VER = 3;
+		final int SALIR_L = 0;
+
+		int opcion_menuL ;
+
+		do {
+			System.out.println("------MENU-------");
+			System.out.println(OPCION_INSERTAR + ". Insertar libro");
+			System.out.println(OPCION_ELIMINAR + ". Eliminar libro");
+			System.out.println(OPCION_VER + ". Ver libros");
+			System.out.println(SALIR_L + ". Salir");
+			System.out.println("Elije una de las opciones");
+			opcion_menuL = Integer.parseInt(scan.nextLine());
+
+			switch (opcion_menuL) {
+			case OPCION_INSERTAR:
+				System.out.println("primera opcion seleccionada\n");
+				break;
+			case OPCION_ELIMINAR:
+				System.out.println("segunda opcion seleccionada\n");
+				break;
+			case OPCION_VER:
+				System.out.println("tercera opcion seleccionada\n");
+				break;
+			case SALIR_L:
+				System.out.println("ADIOS");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+
+		} while (opcion_menuL != SALIR_L);
 	}
 	
 }
