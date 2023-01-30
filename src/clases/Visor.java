289de3258f9a0898108745a9ffb7102f4c31336f
nagor <nagor@DESKTOP-06>
2023-01-30 20:09:59 +0100

    visualizar arraylist de libro

--- src/clases/Visor.java
+++ src/clases/Visor.java
@@ -1,5 +1,13 @@
 package clases;
 
-public class Visor {
+import java.util.ArrayList;
 
+public class Visor {
+	
+public static void mostrarLibros(ArrayList<Libro> libros) {
+	
+	for (Libro li : libros) {
+		System.out.println(li);
+	}
+}
 }
