1080d4fcaf2d13ff0eeec689162b513530a88c06
nagor <nagor@DESKTOP-06>
2023-01-30 20:24:10 +0100

    pedirdatos Libro??

--- src/clases/FormularioDeDatos.java
+++ src/clases/FormularioDeDatos.java
@@ -1,6 +1,20 @@
 package clases;
 
+import java.util.Scanner;
+
 public class FormularioDeDatos {
 
+	public static Libro pedirDatosLibro(Scanner scan) {
+		System.out.println("Cual es el titulo del libro?");
+		String tituloLibro = scan.nextLine();
+		System.out.println("Cual es el autor del libro");
+		String autorLibro = scan.nextLine();
+		System.out.println("Cuantas numeros de paginas tiene ?");
+		int num_pagLibro = Integer.parseInt(scan.nextLine());
+		
+		Libro libro = new Libro();
+		return libro;
+		
+	}
 	
 }
