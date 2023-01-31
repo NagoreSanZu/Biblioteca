c1f469dc64c091be9c3438f44c75375f0708a015
nagor <nagor@DESKTOP-06>
2023-01-31 16:44:52 +0100

    gestor socios??

--- src/clases/GestorSocios.java
+++ src/clases/GestorSocios.java
@@ -1,7 +1,20 @@
 package clases;
 
+import java.util.Scanner;
+
 public class GestorSocios {
-	public static void run() {
+	public static void run(Scanner scan) {
+		int opcionSocios;
 		
-	}
-}
+		do {
+			Menu.menuSocios();
+			opcionSocios=Integer.parseInt(scan.nextLine());
+			
+			switch(opcionSocios){
+			case Menu.VER_SOCIOS:
+			System.out.println("Socios");
+				break;
+			}//fin switch
+		} while(opcionSocios != Menu.SALIR);//fin do
+	}//fin run 
+}//fin clase
