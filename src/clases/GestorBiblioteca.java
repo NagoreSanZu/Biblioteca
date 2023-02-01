5fae538f674347f0e61ac43ca366698301b35394
nagore <znagoresanchez@gmail.com>
2023-02-01 13:17:58 +0100

    base menu

--- src/clases/GestorBiblioteca.java
+++ src/clases/GestorBiblioteca.java
@@ -1,7 +1,34 @@
 package clases;
 
+import java.util.Scanner;
+
 public class GestorBiblioteca {
-	public void run() {
-		
+	public void run(Scanner scan) {
+		int opcionLibro;
+
+		do {
+			Menu.menuPrincipal();
+			opcionLibro=Integer.parseInt(scan.nextLine());
+			
+			switch(opcionLibro){
+			
+			case Menu.GESTIONAR_LIBROS:
+				System.out.println("ins socio");
+				break;
+			
+			case Menu.GESTIONAR_SOCIOS:
+				System.out.println("ins socio");
+				break;
+			
+			case Menu.GESTIONAR_PRESTAMOS:
+				System.out.println("ins socio");
+				break;
+			case Menu.SALIR:
+				System.out.println("agur");
+			
+			default:
+				System.out.println("Opcion incorrecta!");
+			}//fin switch
+		}  while(opcionLibro != Menu.SALIR);//fin do
 	}
 }
